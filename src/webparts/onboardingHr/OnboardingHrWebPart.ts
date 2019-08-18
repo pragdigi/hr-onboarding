da48af2a79b86ceb8d06ed25a0bf0dcca32ebe3d
Krste Kovaceski <krste@pragdigi.com>
2019-08-18 13:34:43 +0200

    change to check context

--- src/webparts/onboardingHr/OnboardingHrWebPart.ts
+++ src/webparts/onboardingHr/OnboardingHrWebPart.ts
@@ -1,31 +1,57 @@
-import { Version } from '@microsoft/sp-core-library';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { Version } from "@microsoft/sp-core-library";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { escape } from '@microsoft/sp-lodash-subset';
+} from "@microsoft/sp-property-pane";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import styles from './OnboardingHrWebPart.module.scss';
-import * as strings from 'OnboardingHrWebPartStrings';
+import styles from "./OnboardingHrWebPart.module.scss";
+import * as strings from "OnboardingHrWebPartStrings";
+import * as microsoftTeams from "@microsoft/teams-js";
+
 
 export interface IOnboardingHrWebPartProps {
   description: string;
 }
 
-export default class OnboardingHrWebPart extends BaseClientSideWebPart<IOnboardingHrWebPartProps> {
+export default class OnboardingHrWebPart extends BaseClientSideWebPart<
+  IOnboardingHrWebPartProps
+> {
+  private teamsContext: microsoftTeams.Context;
+
+  protected onInit(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      if (this.context.microsoftTeams) {
+        this.context.microsoftTeams.getContext(context => {
+          this.teamsContext = context;
+          resolve();
+        });
+      } else {
+        resolve();
+      }
+    });
+  }
 
   public render(): void {
+    let title: string = this.teamsContext ? "Teams" : "SharePoint";
+    let currentLocation: string = this.teamsContext
+      ? `Team: ${this.teamsContext.teamName}`
+      : `site collection ${this.context.pageContext.web.title}`;
     this.domElement.innerHTML = `
-      <div class="${ styles.onboardingHr }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to HR Team</span>
-              <p class="${ styles.subTitle }">This could have infomration about onboarding. It can be added to Teams and SharePoint</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
+      <div class="${styles.onboardingHr}">
+        <div class="${styles.container}">
+          <div class="${styles.row}">
+            <div class="${styles.column}">
+              <span class="${styles.title}">Welcome to HR Team - ${title}</span>
+              <p class="${
+                styles.subTitle
+              }">This could have infomration about onboarding. We are located in: ${currentLocation}</p>
+              <p class="${styles.description}">${escape(
+      this.properties.description
+    )}</p>
+              <a href="https://aka.ms/spfx" class="${styles.button}">
+                <span class="${styles.label}">Learn more</span>
               </a>
             </div>
           </div>
@@ -34,7 +60,7 @@ export default class OnboardingHrWebPart extends BaseClientSideWebPart<IOnboardi
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -48,7 +74,7 @@ export default class OnboardingHrWebPart extends BaseClientSideWebPart<IOnboardi
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
+                PropertyPaneTextField("description", {
                   label: strings.DescriptionFieldLabel
                 })
               ]
